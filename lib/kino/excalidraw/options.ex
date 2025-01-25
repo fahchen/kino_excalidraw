af417600d026d861b7d667d606ceac9a7fd90870
Phil Chen <06fahchen@gmail.com>
2025-01-25 21:44:21 +0800

    feat: handle variable option

--- lib/kino/excalidraw/options.ex
+++ lib/kino/excalidraw/options.ex
@@ -10,6 +10,7 @@ defmodule Kino.Excalidraw.Options do
   | `:view_mode_enabled` | Whether the view mode is enabled. | `false` |
   | `:zen_mode_enabled` | Whether the zen mode is enabled. | `false` |
   | `:grid_mode_enabled` | Whether the grid mode is enabled. | `false` |
+  | `:variable` | The variable name to be used for source code export. | `nil` |
   """
 
   @type t() :: %{
@@ -17,23 +18,25 @@ defmodule Kino.Excalidraw.Options do
           optional(:scroll_to_content) => boolean(),
           optional(:view_mode_enabled) => boolean(),
           optional(:zen_mode_enabled) => boolean(),
-          optional(:grid_mode_enabled) => boolean()
+          optional(:grid_mode_enabled) => boolean(),
+          optional(:variable) => boolean()
         }
 
-  @spec build(Enumerable.t(tuple())) :: t()
-  def build(options) do
+  @all_options ~w[height scroll_to_content view_mode_enabled zen_mode_enabled grid_mode_enabled variable]a
+
+  @spec build(Enumerable.t(tuple()), [atom()]) :: t()
+  def build(options, valid_options \\ @all_options) do
     options
     |> Enum.flat_map(&build_option/1)
+    |> Enum.filter(fn {key, _} -> key in valid_options end)
     |> Map.new()
   end
 
   defp build_option(option_value_tuple)
 
-  defp build_option({"height", value}) when is_integer(value) and value > 0 do
-    [{:height, value}]
-  end
-
-  defp build_option({:height, value}) when is_integer(value) and value > 0 do
+  @option_names [:height, "height"]
+  defp build_option({option, value})
+       when option in @option_names and is_integer(value) and value > 0 do
     [{:height, value}]
   end
 
@@ -48,6 +51,15 @@ defmodule Kino.Excalidraw.Options do
 
     defp build_option({unquote(option), value}) when is_boolean(value) do
       [{unquote(option), value}]
+    end
+  end
+
+  @option_names [:variable, "variable"]
+  defp build_option({option, value}) when is_binary(value) when option in @option_names do
+    if Kino.SmartCell.valid_variable_name?(value) do
+      [{:variable, value}]
+    else
+      []
     end
   end
 
